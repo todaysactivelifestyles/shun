cade80be167e83ff599a3f6f966762ba38ebe10c
Owada Masaya <s1290110@u-aizu.ac.jp>
2022-07-12 20:58:54 +0900

    HoT: initial revision

---
+++ head_or_tail.py
@@ -0,0 +1,16 @@
+import random
+
+print("Tossing a coin...")
+
+heads = 0
+tails = 0
+
+for i in range(1, 4):
+    if random.randint(0,1) == 1:
+        print(f"Round {i}: Heads")
+        heads += 1
+    else:
+        print(f"Round {i}: Tails")
+        tails += 1
+
+print(f"Heads: {heads}, Tails: {tails}")
